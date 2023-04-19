0804c2d26d296b3a392ca87fbcf0d0af94740b2e
ranzan19 <rafaspirina@gmail.com>
2023-04-18 22:41:39 -0300

    add jump
    adicionado pulo ao protótipo

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,21 +6,50 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody2D;
 
-    public Vector2 velocity;
+    //public Vector2 velocity;
+
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
+    public float forceJump = 2;
+
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        HandleJump();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidbody2D.MovePosition(myRigidbody2D.position - velocity * Time.deltaTime);
             myRigidbody2D.velocity = new Vector2(-speed, myRigidbody2D.velocity.y);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody2D.MovePosition(myRigidbody2D.position + velocity * Time.deltaTime);
             myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
+        }
+
+
+        if(myRigidbody2D.velocity.x > 0)
+        {
+            myRigidbody2D.velocity += friction;
+        }
+        else if (myRigidbody2D.velocity.x < 0)
+        {
+            myRigidbody2D.velocity -= friction;
+        }
+    }
+
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * forceJump;
         }
     }
 }
